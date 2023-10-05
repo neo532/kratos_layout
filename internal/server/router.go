8eef63f11fb14a497ad601bea73333e3e364081e
neo <neo532@126.com>
2023-10-05 18:22:46 +0800

    tidy rule

--- internal/server/router.go
+++ internal/server/router.go
@@ -4,7 +4,7 @@ import (
 	http "github.com/go-kratos/kratos/v2/transport/http"
 	"github.com/neo532/kratos_layout/internal/service/api"
 
-	demo "github.com/neo532/kratos_layout/proto/api/demo/v1"
+	demo_v1 "github.com/neo532/kratos_layout/proto/api/demo/v1"
 )
 
 type Router struct {
@@ -12,11 +12,11 @@ type Router struct {
 
 // InitHTTPRouter register HTTP router.
 func InitHTTPRouter(srv *http.Server,
-	dm *api.DemoApi,
+	DemoApi *api.DemoApi,
 ) (r *Router) {
 
 	// router
-	demo.RegisterDemoHTTPServer(srv, dm)
+	demo_v1.RegisterDemoHTTPServer(srv, DemoApi)
 
 	return
 }
